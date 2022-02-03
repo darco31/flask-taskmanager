5b8016149322951719b56378f5b083f5062a6325
Stephen <sdarcy29@hotmail.com>
2022-02-03 15:25:32 +0000

    new commit

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -22,3 +22,13 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    # if request.method == "POST":
+    #     category = Category(category_name=request.form.get("category_name"))
+    #     db.session.add(category)
+    #     db.session.commit()
+    #     return redirect(url_for("categories"))
+    return render_template("edit_category.html")
